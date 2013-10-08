67885d9963af1eef6f0ba7507abda30830ae337e
Jeffrey J. Early <jearly@nwra.com>
2013-10-08 08:51:20 -0700

    Initial Commit

---
+++ QuasigeostrophicEquation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  QuasigeostrophicEquation
+//
+//  Created by Jeffrey J. Early on 10/8/13.
+//  Copyright (c) 2013 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
